48ba85951f6b741e2f9cdfccda127699a3b5a9da
Birendra Bohara <bbirendra693@gmail.com>
2026-04-08 21:08:44 +0545

    feat: add JWT auth controller with signup/login and Zod validation

---
+++ backend/controllers/authController.ts
@@ -0,0 +1,56 @@
+import type { Request, Response } from "express";
+import z from "zod";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import User from "../models/User.js";
+
+// Zod Schemas
+export const signupSchema = z.object({
+    name: z.string().min(2),
+    email:z.string().email(),
+    password: z.string().min(6),
+});
+
+export const loginSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+});
+
+//Signup
+export const signup = async (req:Request, res:Response)=>{
+    try{
+        const data = signupSchema.parse(req.body);
+        const existingUser = await User.findOne({email:data.email});
+        if(existingUser) return res.status(400).json({message:"Email already exists"});
+
+        const hashedPassword = await bcrypt.hash(data.password, 10);
+
+        const user = await User.create({
+            name: data.name,
+            email:data.email,
+            password:hashedPassword,
+        });
+
+        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET!,{expiresIn: "7d"});
+        res.status(201).json({token, user:{id: user._id, name: user.name, email:user.email}});
+    } catch (err:any){
+        res.status(400).json({message:err.message});
+    }
+};
+
+// Login
+export const login = async (req:Request, res:Response)=>{
+    try{
+        const data = loginSchema.parse(req.body);
+        const user = await User.findOne({email: data.email});
+        if(!user) return res.status(400).json({message:"Invalid email"})
+        
+        const isValid = await bcrypt.compare(data.password, user.password);
+        if(!isValid) return res.status(400).json({message:"Invalid password"});
+
+        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET!, {expiresIn:"7d"});
+        res.json({token, user:{id:user._id, name:user.name, email:user.email}});
+    } catch (err:any){
+        res.status(400).json({message:err.message});
+    }
+}
--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv"
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import authRouter from "./routes/authRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -12,8 +13,7 @@ mongoose.connect(process.env.DATABASE_URL!)
 .then(()=> console.log("MongoDB Connected "))
 .catch((err)=>console.error(err));
 
+app.use("/api/auth", authRouter)
+
 const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res)=>res.send("ShopSphere API Running"));
-
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
